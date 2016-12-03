6ec9c3a0571a622b28b02e3a5b5c31141d786e12
kkosos <hduek153642@gmail.com>
2016-12-03 15:42:59 +0800

    asbs

--- routes/apis.js
+++ routes/apis.js
@@ -1,22 +1,47 @@
+require('../lib/db');
+
 var express = require('express');
 var router = express.Router();
+
+//db setting
+var mongoose = require('mongoose');
+var expanda_account=mongoose.model('expanda_account')
 
 
 //register
 
 router.post('/reg',function(req,res,next) {
     //check if input username or passwd
-    /*if((!req.body.username)||(!req.body.passwd))
+    if((!req.body.username)||(!req.body.passwd))
     {
         res.redirect('/users/reg');
         return;
     }
-    res.send("h")
+
     res.redirect('/')
 
-*/
+
     
 }
 );
+
+
+router.post('/login',function(req,res,next) {
+    //check if input username or passwd
+    if((!req.body.username)||(!req.body.passwd))
+    {
+        res.redirect('/users/login');
+        return;
+    }
+    res.session.name = req.body.username;
+    res.session.passwd = req.body.passwd;
+    req.session.logined=true;
+    res.redirect('/')
+
+
+    
+}
+);
+
 
 module.exports = router;
