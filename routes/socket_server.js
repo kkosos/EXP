19790bf52c7c37b3fe888a85d7e7fd97543e5762
kkosos <hduek153642@gmail.com>
2017-01-04 15:23:59 +0800

    example done

--- routes/socket_server.js
+++ routes/socket_server.js
@@ -2,9 +2,12 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/abc',function(req,res){ res.render('socket/socketfile');                                   
+router.get('/abc',function(req,res){ 
+  if(!req.session.name||!req.session.logined){res.redirect('/');}
+  
+  res.render('socket/socketfile',{id:req.session.name});                                   
                                    
-                                   });
+});
 
 
 
