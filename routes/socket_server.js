8c66ea68c60516dddf50ad19c99954b33cc8627a
kkosos <hduek153642@gmail.com>
2017-01-05 18:20:55 +0800

    V1.0

--- routes/socket_server.js
+++ routes/socket_server.js
@@ -2,8 +2,8 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/abc',function(req,res){ 
-  if(!req.session.name||!req.session.logined){res.redirect('/');}
+router.get('/chatroom',function(req,res){ 
+  if(!req.session.name||!req.session.logined){res.render('socket/entertologin');return;}
   
   res.render('socket/socketfile',{id:req.session.name});                                   
                                    
