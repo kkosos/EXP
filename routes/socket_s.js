610cfae9afacfa7f4719b4e321b0d4744b580d8b
kkosos <hduek153642@gmail.com>
2017-01-05 14:27:42 +0800

    done for socket

--- routes/socket_s.js
+++ routes/socket_s.js
@@ -1,4 +1,4 @@
-
+const fs = require('fs');
 exports = module.exports =function(io){
 	
 		io.on( "connection", function( socket )
@@ -8,11 +8,31 @@ exports = module.exports =function(io){
 			socket.on ('disconnect', function(){console.log('user disconnected');});
 	
 			socket.on('chat message', function(msg){ console.log('message: ' + msg);
-
+												   
 												   io.emit('chat message', msg);
 
 												   });
 
+		    socket.on('image choose', function(msg){
+				
+				console.log('img number: ' + msg);
+				if(msg=="img1"){
+					fs.readFile('./public/images/01.png', function (err, buf) {
+					if (err) throw err;
+					io.emit('image show', {image:true,buffer:buf.toString('base64')});
+					});
+				}
+				else{
+					fs.readFile('./public/images/02.png', function (err, buf) {
+					if (err) throw err;
+					io.emit('image show', {image:true,buffer:buf.toString('base64')});
+					});
+				
+				}
+			
+			});
+		
+		
 		});
 
 }
